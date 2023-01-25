644e07653a97bfc562ce6e6187be0d6cd18711d2
edgarmc100 <edgarmc100@gmail.com>
2023-01-25 14:55:15 -0600

    Adds comments in the useMousePosition custom hook

--- src/hooks/useMousePosition.js
+++ src/hooks/useMousePosition.js
@@ -1,4 +1,4 @@
-// We can use another hooks  insinde our ones
+// We can use another hooks  insinde our ones and we do it by import them as we do in any another component.
 import { useState, useEffect } from 'react'
 /*
   The porpose of this hook is to use the mouse position in an state,
@@ -7,8 +7,18 @@ import { useState, useEffect } from 'react'
   for make our pages more dinamyc and interactive.
 */
 export const useMousePosition = () => {
+/*
+  We declare the state in this part of the function accomplish the rules,
+  this state is where will be stored the most important data of this custom hook.
+*/
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  /*
+  Inside this custom hook We can create all the function that are needed,
+  same that if we don't return them they will be private,
+  but we can also return them, which in case of needing it
+  will allow us to share the treatment of the data with our component .
+*/
   const handleMouseEvent = (event) => {
     console.log(event)
     const { clientX, clientY } = event
@@ -17,11 +27,19 @@ export const useMousePosition = () => {
       y: clientY
     })
   }
-
+  /*
+  We can use 'useEffect'. This is usefull, in this case
+  remember delete the listener inside of a anonymous
+  function that is returned in the end, this is a good
+  practice for don't hang the browser.
+*/
   useEffect(() => {
     window.addEventListener('mousemove', handleMouseEvent)
     return () => window.removeEventListener('mousemove', handleMouseEvent)
   }, [])
 
+  /*
+    Finally we return the data to be reused by the component in a simplified way, abstract and elegant.
+  */
   return position
 }
